6cdab5d391a97580e9383672acab4b5b2e7a4908
Ratul Hossain <simanto5858@gmail.com>
2025-12-11 05:03:48 -0600

    Update database connection settings for deployment

--- config/config.php
+++ config/config.php
@@ -1,14 +1,14 @@
 <?php
 // Detect if running on a local development environment
 // LOCAL SETTINGS (XAMPP)
-    $server_name = "localhost";
-    $user_name = "root";       // XAMPP default user
+    $server_name = "sql211.infinityfree.com";
+    $user_name = "if0_40655840";       // XAMPP default user
     $password = "";            // XAMPP default password (empty)
-    $db_name  = "RH-coffee-shop";   // Your local database name (phpMyAdmin)
+    $db_name  = "if0_40655840_rh_coffee_shop";   // Your local database name (phpMyAdmin)
 
     // Local URLs (adjust to your folder name in htdocs)
-    define("url", "http://localhost/RH-coffee-shop");
-    define("ADMINURL", "http://localhost/RH-coffee-shop/admin-panel");
+    define("url", "http://rh-coffee-shop.rf.gd/RH-coffee-shop");
+    define("ADMINURL", "http://rh-coffee-shop.rf.gd/RH-coffee-shop/admin-panel");
 
 // Create MySQL connection
 $conn = mysqli_connect($server_name, $user_name, $password, $db_name);
